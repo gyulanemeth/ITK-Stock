2855d39f2ebc76c34982d4e102227f4eb459bda6
nemgy <gyula.nemeth@itk.ppke.hu>
2012-09-13 10:21:17 +0200

    First commit

---
+++ src/hu/ppke/itk/itkStock/server/TestMain.java
@@ -0,0 +1,13 @@
+package hu.ppke.itk.itkStock.server;
+
+public class TestMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("ITK-Stock server");
+
+	}
+
+}
